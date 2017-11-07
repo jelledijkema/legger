f22c6b5abb602614e61ca701d1245d7f47f764c3
Bastiaan Roos <bastiaan@roosgeo.nl>
2017-11-07 14:06:39 +0100

    directory structure and hello world with example

---
+++ remote_debugger_settings.py
@@ -0,0 +1,16 @@
+
+
+try:
+    import pydevd
+except ImportError:
+    import sys
+    sys.path.append('C:\\Program Files\\JetBrains\\PyCharm 2017.2\\debug-eggs\\pycharm-debug.egg')
+    import pydevd
+
+
+pydevd.settrace('localhost',
+                port=3106,
+                stdoutToServer=True,
+                stderrToServer=True,
+                suspend=False,
+                trace_only_current_thread=True)
