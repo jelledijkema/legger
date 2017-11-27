a132bfe1462206079503d0647b32c2b10976f0a7
Bastiaan Roos <bastiaan@roosgeo.nl>
2017-11-27 16:19:12 +0100

    first version join and read 3di results, including sql alchemy database definition

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # legger
+
+
+
+## Installation
+
+The current version depends on the ThreeDiToolbox.
+
+Put the libary geometry_tools (https://github.com/GetThePointGit/geometry_tools)in the external directory
+
+
---
+++ tests/test_read_tdi_result.py
@@ -0,0 +1,62 @@
+import unittest
+import os.path
+from legger.utils.read_tdi_results import read_tdi_results, write_tdi_results_to_db
+from legger.sql_models.legger_views import create_legger_views
+from pyspatialite import dbapi2 as dbapi
+
+test_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
+
+class TestReadTdiResults(unittest.TestCase):
+
+    def setUp(self):
+        testdir = os.path.join(
+            os.path.dirname(__file__),
+            'data',
+            'tdimodel'
+        )
+
+        self.model_db = os.path.join(
+            testdir,
+            'hhw_1d2d.sqlite'
+        )
+
+        self.result_db = os.path.join(
+            testdir,
+            'subgrid_map.sqlite1'
+        )
+
+        self.result_nc = os.path.join(
+            testdir,
+            'subgrid_map.nc'
+        )
+
+        self.legger_db = os.path.join(
+            os.path.dirname(__file__),
+            'data',
+            'test_spatialite.sqlite'
+        )
+
+
+    def test_x(self):
+
+        result = read_tdi_results(
+            self.model_db,
+            self.result_db,
+            self.result_nc,
+            self.legger_db
+        )
+
+        write_tdi_results_to_db(result,
+                                self.legger_db)
+
+
+
+    def test_create_views(self):
+        con_legger = dbapi.connect(self.legger_db)
+
+        create_legger_views(con_legger)
+
+
+
+
