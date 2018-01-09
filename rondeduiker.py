cc8d7d4b57e3e65aaf3a526bb6edcf7bbb903fd2
rvanderh-hhnk <rvanderh-hhnk@snapunit.com>
2018-01-09 15:07:31 +0100

    foutje hersteld
    eerste foutjes er al uitgehaald.

--- rondeduiker.py
+++ rondeduiker.py
@@ -127,7 +127,7 @@ def wrijvingsverlies(mykm,mynop,mynom,myduikerlengte):
 	(\
 		pow(mykm,2)\
 		*\
-		pow((float(thisnop)/thisnom),float(4)/3)\
+		pow((float(mynop)/mynom),float(4)/3)\
 	)
 	return thiswv
 
@@ -166,53 +166,3 @@ def maxdebiet_metsloot(mywc,mynop,myopstuwing):
 	thismaxdebiet = mywc*mynop\
 		    *pow(2*9.81*myopstuwing,0.5)
 	return thismaxdebiet
-
-# tmp variabelen
-thisdiameter = 1
-thistypemateriaal = 'spiwel'
-thisbod = -3.7	#binnen onderkant duiker
-thiswaterpeil = -2.9
-thisslootbodemhoogte = -3.6
-thisslootbodembreedte = 1 
-thistalud = 1.5
-thisaantalknikken = 1 
-thisknikhoek = 10
-thisaantalbochten = 0 
-thisbochthoek = 10
-thisduikerlengte = 18
-thisopstuwing = 0.253
-thisdebiet = 0.253
-
-thisseg = segmentberekening(thisdiameter,thiswaterpeil,thisbod)
-thissec = sectorberekening(thisdiameter,thiswaterpeil,thisbod)
-thiskm = kmwaarde(thistypemateriaal,thisdiameter)
-thisnop = natteoppervlak_cirkel(thisdiameter,thiswaterpeil,thisbod)
-thisnom = natteomtrek_cirkel(thisdiameter,thiswaterpeil,thisbod)
-thiskv = knikverlies(thisaantalknikken,thisknikhoek)
-thisbv = bochtverlies(thisaantalbochten,thisbochthoek)
-thisuv = uittreeverlies(thisnop,thisslootbodemhoogte,thisslootbodembreedte,thistalud,thiswaterpeil)
-thiswv = wrijvingsverlies(thiskm,thisnop,thisnom,thisduikerlengte)
-thiswcsloot = weerstandscoefficient_metsloot(thisuv,thiskv,thisbv,thiswv)
-thiswcnosloot = weerstandscoefficient_zondersloot(thiskv,thisbv,thiswv)
-thisopstuwsloot = opstuwing_metsloot(thiswcsloot,thisnop,thisdebiet)
-thisopstuwnosloot = opstuwing_zondersloot(thiswcnosloot,thisnop,thisdebiet)
-thismaxdbsloot = maxdebiet_metsloot(thiswcsloot,thisnop,thisopstuwing)
-thismaxdbnosloot = maxdebiet_zondersloot(thiswcnosloot,thisnop,thisopstuwing)
-
-print """\
-	segmentberekening: %s
-	sectorberekening: %s
-	kmwaarde: %s
-	natteoppervlak: %s
-	natteomtrek: %s
-	uittreeverlies: %s
-	weerstandcoefficient met sloot: %s
-	weerstandcoefficient zonder sloot: %s
-	knikverlies: %s
-	bochtverlies: %s
-	wrijvingsverlies: %s
-	max.debiet zonder sloot: %s
-	opstuwing zonder sloot: %s
-	max.debiet met sloot: %s
-	opstuwing met sloot: %s\
-"""%(thisseg,thissec,thiskm,thisnop,thisnom,thisuv,thiswcsloot,thiswcnosloot,thiskv,thisbv,thiswv,thismaxdbnosloot,thisopstuwnosloot,thismaxdbsloot,thisopstuwsloot)
