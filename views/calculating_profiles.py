9270a256b0b17adb7da0d080a9e9249189d9beb6
Bastiaan Roos <bastiaan@roosgeo.nl>
2021-04-07 15:23:03 +0200

    remove unused method 3di result import

--- views/calculating_profiles.py
+++ views/calculating_profiles.py
@@ -6,11 +6,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import os
 import time
 
-from PyQt5.QtCore import QSettings
-from PyQt5.QtWidgets import QFileDialog
 from legger.sql_models.legger import BegroeiingsVariant, HydroObject
 from legger.sql_models.legger_database import LeggerDatabase
 from legger.sql_models.legger_database import load_spatialite
@@ -18,7 +15,6 @@ from legger.utils.calc_gradient import calc_gradient
 from legger.utils.profile_match_a import doe_profinprof, maaktabellen
 from legger.utils.read_tdi_results import (get_timestamps, read_tdi_culvert_results, read_tdi_results,
                                            write_tdi_culvert_results_to_db, write_tdi_results_to_db)
-from legger.utils.read_tdi_results_from_shape import read_tdi_results_from_shape
 from legger.utils.redirect_flows_to_main_branches import redirect_flows
 from legger.utils.snap_points import snap_points
 from legger.utils.theoretical_profiles import create_theoretical_profiles, write_theoretical_profile_results_to_db
@@ -160,7 +156,7 @@ class ProfileCalculationWidget(QWidget):  # , FORM_CLASS):
 
         self.box_step1.addWidget(self.msg_upper_row)
 
-    def execute_step1b(self):
+    def execute_step1(self):
         """
        Eerste stap in klaarzetten van de data:
         - update legger database met data vanuit de netcdf (koppeling debieten van gekozen tijdstap)
@@ -239,68 +235,6 @@ class ProfileCalculationWidget(QWidget):  # , FORM_CLASS):
                 self.feedbackmessage = self.feedbackmessage + "\nFout, 3Di culvert resultaten niet weggeschreven."
             finally:
                 self.feedbacktext.setText(self.feedbackmessage)
-
-    def select_flow_result_file(self):
-        """
-        Select a dump or export of the DAMO database (.gdb)
-        :return:
-        """
-        settings = QSettings('leggertool', 'filepaths')
-        try:
-            init_path = settings.value('last_used_flow_result_path', type=str)
-        except:
-            init_path = os.path.expanduser("~")  # get path to respectively "user" folder
-
-        result_datasource = QFileDialog.getOpenFileName(
-            self,
-            'Selecteer debiet resultaten bestand',
-            init_path)
-
-        if result_datasource:
-            settings.setValue('last_used_flow_result_path',
-                              os.path.dirname(result_datasource[0]))
-        return result_datasource[0]
-
-    def execute_step1(self):
-        """
-       Eerste stap in klaarzetten van de data:
-        - update legger database met data vanuit bestand met debieten (geopackage)
-        returns: None
-        """
-
-        file_path = self.select_flow_result_file()
-
-        # read 3di channel results
-        result = None
-        try:
-            result = read_tdi_results_from_shape(
-                file_path,
-                'BWN_HR_channels_v2',
-                self.polder_datasource
-            )
-            self.feedbackmessage += "\nDatabases zijn gekoppeld."
-        except Exception as e:
-            self.feedbackmessage = self.feedbackmessage + \
-                                   "\nFout in wegschrijven 3Di resultaten naar polder database. " + \
-                                   str(e)
-
-        if result is not None:
-            try:
-                # write 3di channel result to legger spatialite
-                write_tdi_results_to_db(
-                    result,
-                    self.polder_datasource)
-
-                self.feedbackmessage = self.feedbackmessage + "\n3Di resultaten weggeschreven naar polder database."
-            except Exception as e:
-                self.feedbackmessage = self.feedbackmessage + \
-                                       "\nFout in wegschrijven 3Di resultaten naar polder database. " + \
-                                       str(e)
-            finally:
-                self.feedbacktext.setText(self.feedbackmessage)
-        else:
-            self.feedbackmessage = self.feedbackmessage + "\nFout in wegschrijven 3Di resultaten naar polder database."
-            return
 
     def execute_redirect_flows(self):
 
